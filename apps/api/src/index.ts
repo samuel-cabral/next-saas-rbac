c3074730c2b54a7b95d54573b211e32204284e63
Samuel Cabral <samuelcabral.mail@gmail.com>
2024-10-03 23:21:57 -0300

    Setup das permissões - Otimizando a tipagem das permissões

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,12 +1,13 @@
 import { defineAbilityFor } from '@saas/auth'
+import console from 'console'
 
 const ability = defineAbilityFor({ role: 'MEMBER' })
 
-const userCanInviteSomeoneElse = ability.can('invite', 'User')
+// const userCanInviteSomeoneElse = ability.can('invit e', 'User')
 const userCanDeleteOtherUsers = ability.can('delete', 'User')
 
 const userCannotDeleteOtherUsers = ability.cannot('delete', 'User')
 
-console.log(userCanInviteSomeoneElse)
+// console.log(userCanInviteSomeoneElse)
 console.log(userCanDeleteOtherUsers)
 console.log(userCannotDeleteOtherUsers)
